cda9ddf9cb02cf26aacc98ccd8cfa531657f9cc7
Minjo Park <consistent.312@gmail.com>
2025-02-07 20:35:09 +0900

    DexStyle styled component 분리

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,32 +1,20 @@
 import React from "react";
-import styled from "styled-components";
 import Dashboard from "../components/Dashboard";
 import Header from "../components/Header";
 import PokemonList from "../components/PokemonList";
+import { DexStyle as S } from "../styled-component/DexStyle";
 
 const Dex = () => {
   /** 포켓몬 도감 UI */
   return (
-    <DexContainer>
+    <S.DexContainer>
       <Header />
-      <Container>
+      <S.Container>
         <Dashboard />
         <PokemonList />
-      </Container>
-    </DexContainer>
+      </S.Container>
+    </S.DexContainer>
   );
 };
 
 export default Dex;
-
-/** styled component */
-const DexContainer = styled.div`
-  background-color: #121212;
-  min-height: 100vh;
-`;
-
-const Container = styled.div`
-  margin: 0 3rem 0 3rem;
-  padding: 1rem 0 5rem 0;
-  min-height: auto;
-`;
---
+++ src/styled-component/DexStyle.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const DexStyle = {
+  DexContainer: styled.div`
+    background-color: #121212;
+    min-height: 100vh;
+  `,
+
+  Container: styled.div`
+    margin: 0 3rem 0 3rem;
+    padding: 1rem 0 5rem 0;
+    min-height: auto;
+  `,
+};
